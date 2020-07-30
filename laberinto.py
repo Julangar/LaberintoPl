7abb032deff7b021cbf82ffa543732d4218ce9d1
Julangar <jagarciaguer@msn.com>
2020-07-30 12:25:44 -0500

    Casi listo

--- laberinto.py
+++ laberinto.py
@@ -1,10 +1,17 @@
 from pyswip import Prolog
 import numpy as np
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("Laberinto")
+wn.setup(700,700)
+
 prolog = Prolog()
 prolog.consult("laberinto.pl")
 
 
-
+#######################################Trasformar txt a una matriz y encontrar la solucion del laberinto############
 def leer(texto):
     if texto == None:
         pass
@@ -36,163 +43,86 @@ def eliminarespacios(lista):
                 del lista[i]
     return lista
 
-def conectar1(lista):
-        if lista[1]!= "|": 
-            if lista[2]!= "|":       
-                return prolog.assertz("conect("+lista[1]+", "+lista[2]+")")
-
-def conectar2(lista):
-        if lista[2]!= "|": 
-            if lista[3]!= "|":       
-                return prolog.assertz("conect("+lista[2]+", "+lista[3]+")")
-
-def conectar3(lista):
-        if lista[3]!= "|": 
-            if lista[4]!= "|":       
-                return prolog.assertz("conect("+lista[3]+", "+lista[4]+")")
-
-def conectar4(lista):
-        if lista[4]!= "|": 
-            if lista[5]!= "|":       
-                return prolog.assertz("conect("+lista[4]+", "+lista[5]+")")
-
-def conectar5(lista):
-        if lista[5]!= "|": 
-            if lista[6]!= "|":       
-                return prolog.assertz("conect("+lista[5]+", "+lista[6]+")")
-     
-def conectar6(lista):
-        if lista[6]!= "|": 
-            if lista[7]!= "|":       
-                return prolog.assertz("conect("+lista[6]+", "+lista[7]+")")
-
-def conectar7(lista):
-        if lista[7]!= "|": 
-            if lista[8]!= "|":       
-                return prolog.assertz("conect("+lista[7]+", "+lista[8]+")")
-
-def conectar8(lista):
-        if lista[8]!= "|": 
-            if lista[9]!= "|":       
-                return prolog.assertz("conect("+lista[8]+", "+lista[9]+")")
-
-def conectar9(lista):
-        if lista[9]!= "|": 
-            if lista[10]!= "|":       
-                return prolog.assertz("conect("+lista[9]+", "+lista[10]+")")
+def conectar(lista):
+    if lista == []:
+        return []
+    else:
+        for i in range(len(lista)):
+            if lista[i]!= "|": 
+                if lista[i+1]!= "|":       
+                    prolog.assertz("conect("+lista[i]+", "+lista[i+1]+")")
+    return lista
                     
 
-print(leer(""))
-conectar1(eliminarespacios(revisarcolumna(leer(""),[],2)))
-conectar2(eliminarespacios(revisarcolumna(leer(""),[],2)))
-conectar3(eliminarespacios(revisarcolumna(leer(""),[],2)))
-conectar4(eliminarespacios(revisarcolumna(leer(""),[],2)))
-conectar5(eliminarespacios(revisarcolumna(leer(""),[],2)))
-conectar6(eliminarespacios(revisarcolumna(leer(""),[],2)))
-conectar7(eliminarespacios(revisarcolumna(leer(""),[],2)))
-conectar8(eliminarespacios(revisarcolumna(leer(""),[],2)))
-conectar9(eliminarespacios(revisarcolumna(leer(""),[],2)))
-conectar1(eliminarespacios(revisarcolumna(leer(""),[],4)))
-conectar2(eliminarespacios(revisarcolumna(leer(""),[],4)))
-conectar3(eliminarespacios(revisarcolumna(leer(""),[],4)))
-conectar4(eliminarespacios(revisarcolumna(leer(""),[],4)))
-conectar5(eliminarespacios(revisarcolumna(leer(""),[],4)))
-conectar6(eliminarespacios(revisarcolumna(leer(""),[],4)))
-conectar7(eliminarespacios(revisarcolumna(leer(""),[],4)))
-conectar8(eliminarespacios(revisarcolumna(leer(""),[],4)))
-conectar9(eliminarespacios(revisarcolumna(leer(""),[],4)))
-conectar1(eliminarespacios(revisarcolumna(leer(""),[],6)))
-conectar2(eliminarespacios(revisarcolumna(leer(""),[],6)))
-conectar3(eliminarespacios(revisarcolumna(leer(""),[],6)))
-conectar4(eliminarespacios(revisarcolumna(leer(""),[],6)))
-conectar5(eliminarespacios(revisarcolumna(leer(""),[],6)))
-conectar6(eliminarespacios(revisarcolumna(leer(""),[],6)))
-conectar7(eliminarespacios(revisarcolumna(leer(""),[],6)))
-conectar8(eliminarespacios(revisarcolumna(leer(""),[],6)))
-conectar9(eliminarespacios(revisarcolumna(leer(""),[],6)))
-conectar1(eliminarespacios(revisarcolumna(leer(""),[],8)))
-conectar2(eliminarespacios(revisarcolumna(leer(""),[],8)))
-conectar3(eliminarespacios(revisarcolumna(leer(""),[],8)))
-conectar4(eliminarespacios(revisarcolumna(leer(""),[],8)))
-conectar5(eliminarespacios(revisarcolumna(leer(""),[],8)))
-conectar6(eliminarespacios(revisarcolumna(leer(""),[],8)))
-conectar7(eliminarespacios(revisarcolumna(leer(""),[],8)))
-conectar8(eliminarespacios(revisarcolumna(leer(""),[],8)))
-conectar9(eliminarespacios(revisarcolumna(leer(""),[],8)))
-conectar1(eliminarespacios(revisarcolumna(leer(""),[],10)))
-conectar2(eliminarespacios(revisarcolumna(leer(""),[],10)))
-conectar3(eliminarespacios(revisarcolumna(leer(""),[],10)))
-conectar4(eliminarespacios(revisarcolumna(leer(""),[],10)))
-conectar5(eliminarespacios(revisarcolumna(leer(""),[],10)))
-conectar6(eliminarespacios(revisarcolumna(leer(""),[],10)))
-conectar7(eliminarespacios(revisarcolumna(leer(""),[],10)))
-conectar8(eliminarespacios(revisarcolumna(leer(""),[],10)))
-conectar9(eliminarespacios(revisarcolumna(leer(""),[],10)))
-conectar1(eliminarespacios(revisarcolumna(leer(""),[],12)))
-conectar2(eliminarespacios(revisarcolumna(leer(""),[],12)))
-conectar3(eliminarespacios(revisarcolumna(leer(""),[],12)))
-conectar4(eliminarespacios(revisarcolumna(leer(""),[],12)))
-conectar5(eliminarespacios(revisarcolumna(leer(""),[],12)))
-conectar6(eliminarespacios(revisarcolumna(leer(""),[],12)))
-conectar7(eliminarespacios(revisarcolumna(leer(""),[],12)))
-conectar8(eliminarespacios(revisarcolumna(leer(""),[],12)))
-conectar9(eliminarespacios(revisarcolumna(leer(""),[],12)))
-conectar1(eliminarespacios(revisarfila(leer(""),[],2)))
-conectar2(eliminarespacios(revisarfila(leer(""),[],2)))
-conectar3(eliminarespacios(revisarfila(leer(""),[],2)))
-conectar4(eliminarespacios(revisarfila(leer(""),[],2)))
-conectar5(eliminarespacios(revisarfila(leer(""),[],2)))
-conectar6(eliminarespacios(revisarfila(leer(""),[],2)))
-conectar7(eliminarespacios(revisarfila(leer(""),[],2)))
-conectar8(eliminarespacios(revisarfila(leer(""),[],2)))
-conectar9(eliminarespacios(revisarfila(leer(""),[],2)))
-conectar1(eliminarespacios(revisarfila(leer(""),[],4)))
-conectar2(eliminarespacios(revisarfila(leer(""),[],4)))
-conectar3(eliminarespacios(revisarfila(leer(""),[],4)))
-conectar4(eliminarespacios(revisarfila(leer(""),[],4)))
-conectar5(eliminarespacios(revisarfila(leer(""),[],4)))
-conectar6(eliminarespacios(revisarfila(leer(""),[],4)))
-conectar7(eliminarespacios(revisarfila(leer(""),[],4)))
-conectar8(eliminarespacios(revisarfila(leer(""),[],4)))
-conectar9(eliminarespacios(revisarfila(leer(""),[],4)))
-conectar1(eliminarespacios(revisarfila(leer(""),[],6)))
-conectar2(eliminarespacios(revisarfila(leer(""),[],6)))
-conectar3(eliminarespacios(revisarfila(leer(""),[],6)))
-conectar4(eliminarespacios(revisarfila(leer(""),[],6)))
-conectar5(eliminarespacios(revisarfila(leer(""),[],6)))
-conectar6(eliminarespacios(revisarfila(leer(""),[],6)))
-conectar7(eliminarespacios(revisarfila(leer(""),[],6)))
-conectar8(eliminarespacios(revisarfila(leer(""),[],6)))
-conectar9(eliminarespacios(revisarfila(leer(""),[],6)))
-conectar1(eliminarespacios(revisarfila(leer(""),[],8)))
-conectar2(eliminarespacios(revisarfila(leer(""),[],8)))
-conectar3(eliminarespacios(revisarfila(leer(""),[],8)))
-conectar4(eliminarespacios(revisarfila(leer(""),[],8)))
-conectar5(eliminarespacios(revisarfila(leer(""),[],8)))
-conectar6(eliminarespacios(revisarfila(leer(""),[],8)))
-conectar7(eliminarespacios(revisarfila(leer(""),[],8)))
-conectar8(eliminarespacios(revisarfila(leer(""),[],8)))
-conectar9(eliminarespacios(revisarfila(leer(""),[],8)))
-conectar1(eliminarespacios(revisarfila(leer(""),[],10)))
-conectar2(eliminarespacios(revisarfila(leer(""),[],10)))
-conectar3(eliminarespacios(revisarfila(leer(""),[],10)))
-conectar4(eliminarespacios(revisarfila(leer(""),[],10)))
-conectar5(eliminarespacios(revisarfila(leer(""),[],10)))
-conectar6(eliminarespacios(revisarfila(leer(""),[],10)))
-conectar7(eliminarespacios(revisarfila(leer(""),[],10)))
-conectar8(eliminarespacios(revisarfila(leer(""),[],10)))
-conectar9(eliminarespacios(revisarfila(leer(""),[],10)))
-conectar1(eliminarespacios(revisarfila(leer(""),[],12)))
-conectar2(eliminarespacios(revisarfila(leer(""),[],12)))
-conectar3(eliminarespacios(revisarfila(leer(""),[],12)))
-conectar4(eliminarespacios(revisarfila(leer(""),[],12)))
-conectar5(eliminarespacios(revisarfila(leer(""),[],12)))
-conectar6(eliminarespacios(revisarfila(leer(""),[],12)))
-conectar7(eliminarespacios(revisarfila(leer(""),[],12)))
-conectar8(eliminarespacios(revisarfila(leer(""),[],12)))
-conectar9(eliminarespacios(revisarfila(leer(""),[],12)))
+conectar(eliminarespacios(revisarcolumna(leer(""),[],2)))
+conectar(eliminarespacios(revisarcolumna(leer(""),[],4)))
+conectar(eliminarespacios(revisarcolumna(leer(""),[],6)))
+conectar(eliminarespacios(revisarcolumna(leer(""),[],8)))
+conectar(eliminarespacios(revisarcolumna(leer(""),[],10)))
+conectar(eliminarespacios(revisarcolumna(leer(""),[],12)))
+conectar(eliminarespacios(revisarfila(leer(""),[],2)))
+conectar(eliminarespacios(revisarfila(leer(""),[],4)))
+conectar(eliminarespacios(revisarfila(leer(""),[],6)))
+conectar(eliminarespacios(revisarfila(leer(""),[],8)))
+conectar(eliminarespacios(revisarfila(leer(""),[],10)))
+conectar(eliminarespacios(revisarfila(leer(""),[],12)))
 
 
-for sol in prolog.query("sol"):
-    print(sol)
+
+for sol in prolog.query("sol()"):
+    for clave,valor in sol.items():
+        print (valor)
+
+print ("  ")
+
+
+
+"""slu = list(sol.items())
+print(slu[0])"""
+
+
+
     
+        
+   
+##########################################Dibujar Laberinto##############################
+class Pen(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.speed(0)
+
+niveles = []
+
+
+nivel_1 = []
+nivel_1.append(revisarfila(leer(""),[],1))
+nivel_1.append(revisarfila(leer(""),[],2))
+nivel_1.append(revisarfila(leer(""),[],4))
+nivel_1.append(revisarfila(leer(""),[],6))
+nivel_1.append(revisarfila(leer(""),[],8))
+nivel_1.append(revisarfila(leer(""),[],10))
+nivel_1.append(revisarfila(leer(""),[],12))
+nivel_1.append(revisarfila(leer(""),[],13))
+niveles.append(nivel_1)
+
+def iniciar_lab(nivel):
+    for fila in range(len(nivel)):
+        for column in range(len(nivel[fila])):
+            barra = nivel[fila][column]
+            screen_x = -288 + (column * 24)
+            screen_y = 288 - (fila * 24)
+            if barra == "i" or "f":
+                pen.goto(screen_x, screen_y)
+                pen.color("red")
+                pen.stamp()
+            if barra == "|":
+                pen.goto(screen_x, screen_y)
+                pen.color("white")
+                pen.stamp()    
+pen = Pen()
+
+iniciar_lab(niveles[0])
+
